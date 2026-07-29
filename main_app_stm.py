1ce32ef9e6996bf674b5ffe1daf4c8cb87037d17
ritikapatlan63-cmd <ritikapatlan63@gmail.com>
2026-07-29 12:04:56 +0530

    Update main_app_stm.py

--- main_app_stm.py
+++ main_app_stm.py
@@ -51,7 +51,7 @@ with col1:
 # RIGHT COLUMN
 # LLM Description
 # ===================================
-        secrets.st['groq_api']
+        groq_api=secrets.st['groq_api']
         output = llm_app.llm_app(topic, groq_api)
 
         with col2:
